81bfe4bfc8d0112e46ad362813af26abdc1294ee
Michail Antonkopoulos <it21805@hua.gr>
2025-11-07 19:32:39 +0200

    user can search using url wiki's now

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -7,4 +8,15 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def entry(request, title):
+    content=util.get_entry(title)
+    if content is None:
+        return render (request,"encyclopedia/error.html",{
+            "message":"The requested page was not found."
+        })
+    
+    content_html= markdown2.markdown(content)
+    return render(request,"encyclopedia/entry.html",{
+        "title":title,
+        "content":content_html
+    })
