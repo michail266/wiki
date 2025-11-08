c7e8d976d85dff1fee86333d75cd8e17ab8382bf
Michail Antonkopoulos <it21805@hua.gr>
2025-11-08 18:24:51 +0200

    you can now click on index items and change webpage

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,11 +8,12 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 def entry(request, title):
     content=util.get_entry(title)
     if content is None:
         return render (request,"encyclopedia/error.html",{
-            "message":"The requested page was not found."
+            "message":f"The requested {title}  page was not found."
         })
     
     content_html= markdown2.markdown(content)
